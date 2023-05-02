f086aeb4b8c87f83dfe29a55ad0196804f216166
Aansh Basu <96300383+Aansh2003@users.noreply.github.com>
2023-05-02 09:12:07 +0530

    init

---
+++ flask_test.py
@@ -0,0 +1,207 @@
+from flask import Flask, render_template, request, redirect, url_for, session,render_template_string
+from trainmanagement import TrainData
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+login_det = {'admin': 'admin'}
+print(list(login_det.keys()))
+logged_in = []
+
+filehtml = 'templates/home.html'
+
+with open(filehtml) as file:
+    htmlFile = file.read()
+    soup2 = BeautifulSoup(htmlFile,features="lxml")
+
+
+
+my_db = TrainData('ruru','password','localhost')
+schedule_id = []
+
+@app.route('/', methods=['GET'])
+def home():
+    if 'username' in session:
+        print(session['username'])
+        my_string = str(soup2)
+        soup = BeautifulSoup(my_string)
+        head = soup.find("container2")
+        l = my_db.get_user_booking_details(session['username'])
+        print(l)
+        if len(l)==0:
+            message = "No bookings yet."
+            print(message)
+            return render_template('home.html', username=session['username'],info=message)
+        else:
+            for value in l:
+                new_p = soup.new_tag("p",**{'class':'booking'})
+                new_p.string = "Scheduled ID:"+str(value[0])+" Train ID:"+str(value[5])
+                head.append(new_p)
+            return render_template_string(str(soup),username=session['username'])
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        out = my_db.get_user_password(username)
+        print(out)
+        if out == False:
+            return render_template('login.html',error='Invalid username or password.')
+        elif out != password:
+            return render_template('login.html',error='Invalid username or password.')
+        else:
+            session['username'] = username
+            return redirect(url_for('home'))
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        fname = request.form['fname']
+        sname = request.form['sname']
+        phone = request.form['phone']
+        sex = request.form['gender']
+        if sex == 0:
+            gender = 'M'
+        elif sex == 1:
+            gender = 'F'
+        else:
+            gender = 'O'
+        password = request.form['password']
+        out = my_db.set_user_data(username,password,fname,sname,email,phone,gender)
+        print(out)
+        if out != 1:
+            return render_template('register.html',error='Invalid details')
+        print('Succesfully registered'+username)
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/emp_home')
+def emp_home():
+    if 'username' in session:
+        print(session['username'])
+        return render_template('employee_home.html', username=session['username'])
+    return redirect(url_for('emp_login'))
+
+@app.route('/emp_login', methods=['GET', 'POST'])
+def emp_login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        out = my_db.get_employee_password(username)
+        print(out)
+        if out == False:
+            return render_template('employee_login.html',error='Invalid username or password.')
+        elif out != password:
+            return render_template('employee_login.html',error='Invalid username or password.')
+        else:
+            session['username'] = username
+            return redirect(url_for('emp_home'))
+    return render_template('employee_login.html')
+
+@app.route('/book', methods=['GET', 'POST'])
+def book():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['number']
+        trav_from = request.form['start_loc']
+        trav_to = request.form['end_loc']
+        date = request.form['date']
+        no_adults = request.form['adult']
+        no_children = request.form['children']
+        no_infants = request.form['infant']
+        my_list = my_db.get_scheduled_train_details(date,trav_from,trav_to)
+        names = []
+        if len(my_list)==0:
+            error = 'No train found'
+            return render_template('booking-form.html',error=error)
+        elif len(my_list)>=1:
+            names.append(my_list[0][8])
+            scheduled_id = my_list[0][1]
+            if my_db.check_seat_availibility(scheduled_id):
+                if my_db.book_scheduled_train(scheduled_id,session['username'],no_adults,no_children) == True:
+                    message = no_adults+' ac tickets and '+no_children+' non-AC tickets booked.'
+                    return render_template('booking-form.html',message=message)
+                else:
+                    error = 'Train full.'
+                    return render_template('booking-form.html',error=error)
+    return render_template('booking-form.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+@app.route('/emp_logout')
+def emp_logout():
+    session.pop('username', None)
+    return redirect(url_for('emp_login'))
+
+@app.route('/manage',methods=['GET', 'POST'])
+def manage():
+    if request.method == 'POST':
+        id = request.form['schedule']
+        print(id)
+        num = my_db.check_seat_availibility(id)
+        message = 'Booked seats - Non-AC:'+str(num[0])+'  AC:'+str(num[1])
+        return render_template('manage.html',info=message)
+    return render_template('manage.html')
+
+@app.route('/contus')
+def about():
+    return render_template('contus.html')
+
+@app.route('/emp_reg',methods=['POST','GET'])
+def reg():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        fname = request.form['fname']
+        sname = request.form['sname']
+        phone = request.form['phone']
+        sex = request.form['gender']
+        if sex == 0:
+            gender = 'M'
+        elif sex == 1:
+            gender = 'F'
+        else:
+            gender = 'O'
+        password = request.form['password']
+        out = my_db.set_employee_data(username,password,fname,sname,email,phone,gender)
+        print(out)
+        if out != 1:
+            return render_template('register.html',error='Invalid details')
+        print('Succesfully registered'+username)
+        return redirect(url_for('emp_login'))
+    return render_template('emp_reg.html')
+
+@app.route('/schedule',methods=['POST','GET'])
+def schedule():
+    if request.method == 'POST':
+        trainID = request.form['trainID']
+        trainName = request.form['trainName']
+        start = request.form['start']
+        end = request.form['end']
+        schedule_id = my_db.schedule_train(trainID,start,end)
+        print(schedule_id)
+    return render_template('schedule.html')
+
+@app.route('/remove',methods=['POST','GET'])
+def remove():
+    if request.method == 'POST':
+        id = request.form['schedule']
+        num = my_db.check_seat_availibility(id)
+        message = 'Booked seats - Non-AC:'+str(num[0])+'  AC:'+str(num[1])
+        return render_template('remove.html',info=message)
+    return render_template('remove.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+#([('username', 'aa'), ('email', 'aansh.basu@learner.manipal.edu'), ('fname', 'a'), ('sname', 's'), ('phone', 's'), ('gender', '1'), ('password', 'a')])
---
+++ initialize_database.py
@@ -0,0 +1,117 @@
+import mysql.connector
+
+# Initialising Database
+
+dataBase = mysql.connector.connect(
+    host="localhost",
+    user="ruru",
+    passwd="password",
+    allow_local_infile = True,
+    auth_plugin='mysql_native_password'
+)
+
+# Defining Curser Object 
+
+cursorObject = dataBase.cursor()
+
+# User Tables
+
+tblUser = """create table tblUsers (
+    username varchar(20) unique primary key,
+    password varchar(20)
+)"""
+
+tblUserDetails = """create table tblUserDetails (
+    username varchar(20),
+    first_name varchar(20),
+    second_name varchar(20),
+    email_id varchar(50),
+    phone_number numeric(10),
+    gender varchar(1),
+    constraint FK_UserDetails_Users foreign key (username) references tblUsers(username),
+    constraint CHK_valid_email_user check(email_id like '%@%.com'),
+    constraint CHK_valid_phone_number_user check(phone_number >= 1000000000 and phone_number <= 9999999999),
+    constraint CHK_valid_gender_user check(gender in ('F', 'M', 'O'))
+)"""
+
+# Employee Tables
+
+tblEmployees = """create table tblEmployees (
+    emp_username varchar(20) unique primary key,
+    password varchar(20)
+)"""
+
+tblEmployeeDetails = """create table tblEmployeeDetails (
+    emp_username varchar(20),
+    first_name varchar(20),
+    second_name varchar(20),
+    email_id varchar(50),
+    phone_number numeric(10),
+    gender varchar(1),
+    constraint FK_UserDetails_Employees foreign key (emp_username) references tblEmployees(emp_username),
+    constraint CHK_valid_email_employee check(email_id like '%@%.com'),
+    constraint CHK_valid_phone_number_employee check(phone_number >= 1000000000 and phone_number <= 9999999999),
+    constraint CHK_valid_gender_employee check(gender in ('F', 'M', 'O'))
+)"""
+
+# Train table 
+
+tblTains_create = """create table tblTrains (
+    num numeric(5),
+    train_id numeric(10) primary key,
+    train_name varchar(50),
+    start_location varchar(50),
+    end_location varchar(50)
+)"""
+
+tblTrains_adddata = """load data local infile '~/projects/dbs/dataset/All_Indian_Trains.csv'
+into table tblTrains
+fields terminated by ','
+ignore 1 rows"""
+
+tblTains_deletecol = """alter table tblTrains
+drop column num"""
+
+# Train Schedule Table
+
+tblTrainSchedules = """create table tblTrainSchedules (
+    schedule_id int unsigned not null auto_increment primary key,
+    train_id numeric(10),
+    departure_date_time datetime,
+    arrival_date_time datetime,
+    ac_seats int, 
+    ac_seats_occupied int,
+    non_ac_seats int, 
+    non_ac_seats_occupied int,
+    constraint FK_Trains_TrainSchedule foreign key (train_id) references tblTrains(train_id),
+    constraint CHK_valid_train_time check (departure_date_time < arrival_date_time),
+    constraint CHK_valid_seating check((ac_seats > ac_seats_occupied) and (non_ac_seats > non_ac_seats_occupied))
+)"""
+
+# Train Bookings Table
+
+tblBookings = """create table tblBookings (
+    booking_id int unsigned not null auto_increment primary key,
+    username varchar(20),
+    schedule_id int unsigned not null,
+    ac_seats_alloted int,
+    non_ac_seats_alloted int,
+    constraint FK_Users_Bookings foreign key (username) references tblUsers(username),
+    constraint FK_TrainSchedules_Bookings foreign key (schedule_id) references tblTrainSchedules(schedule_id)
+)"""
+
+# Table creation
+
+cursorObject.execute("create database train_management")
+cursorObject.execute("use train_management")
+cursorObject.execute(tblUser)
+cursorObject.execute(tblUserDetails)
+cursorObject.execute(tblEmployees)
+cursorObject.execute(tblEmployeeDetails)
+cursorObject.execute(tblTains_create)
+cursorObject.execute(tblTrains_adddata)
+cursorObject.execute(tblTains_deletecol)
+cursorObject.execute(tblTrainSchedules)
+cursorObject.execute(tblBookings)
+
+
